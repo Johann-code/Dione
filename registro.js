7b26958ec24821e1911bae7043770e31427479af
Johann-code <sebastianas@gmail.com>
2024-11-05 09:51:08 -0500

    cambios cambiosos

--- registro.js
+++ registro.js
@@ -10,18 +10,18 @@ formulario.addEventListener("submit", (e) => {
 })
 
 function grillito() {
-    let usuario = document.getElementById("usuary").value;
+    // let usuario = document.getElementById("usuary").value;
     let fecha_nacimiento = document.getElementById("born").value;
     let email = document.getElementById("carta").value;
     let contraseña = document.getElementById("topSecret").value;
     let comprobante = document.getElementById("clonSecret").value;
-    if (usuario === "") {
-        alert("Los datos deben estar completos.");
-    } else {
-        if (fecha_nacimiento === "") {
+    // if (usuario === "") {
+    //     alert("Los datos deben estar completos.");
+    // } else {
+        if (email === "") {
             alert("Los datos deben estar completos.");
         } else {
-            if (email === "") {
+            if (fecha_nacimiento === "") {
                 alert("Los datos deben estar completos.");
             } else {
                 if (contraseña === "") {
@@ -31,7 +31,16 @@ function grillito() {
                         alert("Los datos deben estar completos.");
                     } else {
                         if (contraseña === comprobante) {
-                            console.log("Datos completos :D");
+                            //CREAR CORREO Y PASSWORD 
+    createUserWithEmailAndPassword(auth, email, contraseña)
+        .then((userCredential) => {
+            alert('Cuenta creada con éxito. A continuación pon un nombr de usuario y sube una foto de perfil.');
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            alert('error');
+            console.log(error.message);
+        })
                         } else {
                             alert("Has escrito mal la confirmación de contraseña")
                         }
@@ -40,16 +49,5 @@ function grillito() {
             }
         }
     }
-
-    //CREAR CORREO Y PASSWORD 
-    createUserWithEmailAndPassword(auth, email, contraseña)
-        .then((userCredential) => {
-            alert('Cuenta creada con éxito. Inicia Sesión.');
-            window.location.href = "index.html";
-        })
-        .catch((error) => {
-            alert('error');
-            console.log(error.message);
-        })
-};
+// };
 
