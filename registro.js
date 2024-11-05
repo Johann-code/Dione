9d8c70b6dbe53f589273afd35d8370c8c6c42b67
Johann-code <sebastianas@gmail.com>
2024-11-05 10:41:14 -0500

    registro actualizado

--- registro.js
+++ registro.js
@@ -10,46 +10,57 @@ formulario.addEventListener("submit", (e) => {
 })
 
 function grillito() {
-    // let usuario = document.getElementById("usuary").value;
+    let usuario = document.getElementById("usuary").value;
     let fecha_nacimiento = document.getElementById("born").value;
     let email = document.getElementById("carta").value;
     let contraseña = document.getElementById("topSecret").value;
     let comprobante = document.getElementById("clonSecret").value;
-    // if (usuario === "") {
-    //     alert("Los datos deben estar completos.");
-    // } else {
-    
+
+
     //Comprobación de datos
-        if (email === "") {
+    if (usuario === "") {
+        alert("Los datos deben estar completos.");
+    } else {
+    if (email === "") {
+        alert("Los datos deben estar completos.");
+    } else {
+        if (fecha_nacimiento === "") {
             alert("Los datos deben estar completos.");
         } else {
-            if (fecha_nacimiento === "") {
+            if (contraseña === "") {
                 alert("Los datos deben estar completos.");
             } else {
-                if (contraseña === "") {
+                if (comprobante === "") {
                     alert("Los datos deben estar completos.");
                 } else {
-                    if (comprobante === "") {
-                        alert("Los datos deben estar completos.");
+                    if (contraseña === comprobante) {
+                        //CREAR CORREO Y PASSWORD 
+                        createUserWithEmailAndPassword(auth, email, contraseña)
+                            .then((userCredential) => {
+                                let user = userCredential.user;
+
+                                //Actualizar el nombre de usuario
+                                updateProfile(user, {
+                                    displayName: nombre
+                                }).then(() => {
+                                    console.log("Se actualizó el nombre a " + user.displayName);
+                                    //Redirigir a la página de inicio
+                                    window.location.href = "index.html";
+                                }).catch((error) => {
+                                    console.log("Error al actualizar el nombre del usuario:", error);
+                                })
+                            })
+                            .catch((error) => {
+                                alert('error');
+                                console.log(error.message);
+                            })
                     } else {
-                        if (contraseña === comprobante) {
-                            //CREAR CORREO Y PASSWORD 
-    createUserWithEmailAndPassword(auth, email, contraseña)
-        .then((userCredential) => {
-            alert('Cuenta creada con éxito. A continuación pon un nombr de usuario y sube una foto de perfil.');
-            window.location.href = "index.html";
-        })
-        .catch((error) => {
-            alert('error');
-            console.log(error.message);
-        })
-                        } else {
-                            alert("Has escrito mal la confirmación de contraseña")
-                        }
+                        alert("Has escrito mal la confirmación de contraseña")
                     }
                 }
             }
         }
     }
-// };
+}
+};
 
